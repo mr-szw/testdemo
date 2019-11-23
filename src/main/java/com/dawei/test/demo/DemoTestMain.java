c829d1aa371806717c2c0d85743335faba6e53bf
sunzhiwei <sunzhiwei@daojia-inc.com>
2019-11-23 17:07:10 +0800

    DEV: 布隆过滤器

--- src/main/java/com/dawei/test/demo/DemoTestMain.java
+++ src/main/java/com/dawei/test/demo/DemoTestMain.java
@@ -1,215 +1,62 @@
 package com.dawei.test.demo;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-
+import com.dawei.test.demo.bloomfilter.BloomFilter;
+import com.dawei.test.demo.bloomfilter.BloomFilter.MisjudgmentRate;
+import java.util.UUID;
 import org.junit.Test;
-import org.springframework.util.CollectionUtils;
-import org.springframework.util.StringUtils;
-
-import com.alibaba.fastjson.JSON;
-import com.dawei.test.demo.utils.EmailSendConfigInfo;
-import com.dawei.test.demo.utils.FileUtil;
-import com.google.common.collect.Lists;
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
-import com.google.zxing.BarcodeFormat;
-import com.google.zxing.EncodeHintType;
-import com.google.zxing.MultiFormatWriter;
-import com.google.zxing.client.j2se.MatrixToImageWriter;
-import com.google.zxing.common.BitMatrix;
-
-import sun.misc.BASE64Encoder;
 
 /**
  * @author Dawei  on 2018/3/25.
  */
 public class DemoTestMain {
 
-    public static final String SERVICE_WORK_PATH = "/home/sinbad/work//workspace/emailTempPath/";
-
-    class Solution {
-
-        public int removeDuplicates(int[] nums) {
-            int length = nums.length;
-            for (int i = 1; i < length; i++) {
-                for (int j = i + 1; j < length; j++) {
-                    if(nums[i] == nums[j]) {
-
-                    }
-                }
-            }
-
-            int numCount = 1;
-            int temp = nums[0];
-
-            int tempIndex = 0;
-            for (int i = 1; i < nums.length - 1; ) {
-                if (temp != nums[i]) {
-                    numCount++;
-                    temp = nums[i];
-                } else {
-                    tempIndex = i;
-                    nums[i] = nums[tempIndex];
-                }
-            }
-            return numCount;
-        }
-    }
-
-
-    public static List<String> getFileNameList(String filePathOnService) throws Throwable{
-        List<String> fileNameList = new ArrayList<>();
-        File file = new File(filePathOnService);
-        File[] files = file.listFiles();
-        if (files != null) {
-            for (File fileInPath : files) {
-                if (fileInPath.isFile()) {
-                    fileNameList.add(fileInPath.getPath());
-                }
-            }
-        }
-        return fileNameList;
-    }
-
-
-    public static List<String> getFilePathList(String filePathOnService) throws Throwable{
-        List<String> filePathList = new ArrayList<>();
-        File file = new File(filePathOnService);
-        File[] files = file.listFiles();
-        if (files != null) {
-            for (File fileInPath : files) {
-                if (fileInPath.isDirectory()) {
-                    filePathList.add(fileInPath.getPath());
-                }
-            }
-        }
-        return filePathList;
-    }
-
-    /**
-     * 获取路径下的文件名 全路径 仅名字
-     */
-    public static List<String> getFileOnlyNameList(String filePathOnService) throws Throwable{
-        List<String> fileOnlyNameList = new ArrayList<>();
-        List<String> fileNameList = getFileNameList(filePathOnService);
-        if (!CollectionUtils.isEmpty(fileNameList)) {
-            for (String allFileName : fileNameList) {
-                fileOnlyNameList.add(allFileName.substring(allFileName.lastIndexOf("/") + 1));
-            }
-        }
-        return fileOnlyNameList;
-    }
-
-
-    public static String generateQRCodeStreamToFile(String filePath, String content, String boardId) {
-        int width = 300;
-        int height = 300;
-        String format = "png";
-        Map<EncodeHintType, Object> hintTypeObjectMap = new HashMap<>();
-        hintTypeObjectMap.put(EncodeHintType.CHARACTER_SET, "utf-8");
-
-        File serviceFlePath;
-        try {
-            serviceFlePath = new File(filePath);
-            if (!serviceFlePath.exists()) {
-                boolean mkdir = serviceFlePath.mkdir();
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        try {
-            //参数分别为：编码内容、编码类型、图片宽度、图片高度，设置参数
-            BitMatrix bitMatrix = new MultiFormatWriter().encode(content, BarcodeFormat.QR_CODE, width, height, hintTypeObjectMap);
-            File outputFile = new File(filePath + "boardQRCode_" + boardId);
-            MatrixToImageWriter.writeToFile(bitMatrix, format, outputFile);
-        }catch(Throwable e) {
-            e.printStackTrace();
-        }
-        return filePath + boardId;
-    }
-
 
     @Test
-    public void madin() throws Throwable {
+    public void testMethod() throws Throwable {
+        BloomFilter bloomFilter = new BloomFilter(100, MisjudgmentRate.MIDDLE, 0.8);
 
-        String a = FileUtil.generateQRCodeStreamToFile("a", "sasa", true, 2);
+        int i = 9000;
+        String dataTemp = UUID.randomUUID().toString();
+        for (;i-- > 0;) {
+            String data = UUID.randomUUID().toString();
+            bloomFilter.add(data);
+        }
+        bloomFilter.add(dataTemp);
 
+        System.out.println(bloomFilter.checkExist(dataTemp));
+
+        int j = 100000;
+        int failedCount = 0;
+        int dataCount = 0;
+        for (;j-- > 0;) {
+
+            String dataA = UUID.randomUUID().toString();
+            boolean checkExist = bloomFilter.checkExist(dataA);
+            dataCount++;
+            if (checkExist ){
+                System.out.println(String.format("Count : %s filedNum = %s ", dataCount , ++failedCount));
+            }
+        }
     }
 
-    // 将图片文件转化为字节数组字符串，并对其进行Base64编码处理
-    public static String getImageBASE64Str(String imgFileAllPath) {
-        if(imgFileAllPath == null) {
-            return "";
-        }
-        InputStream inputStream = null;
-        // 读取图片字节数组
-        byte[] data;
-        try {
-            inputStream = new FileInputStream(imgFileAllPath);
-            data = new byte[inputStream.available()];
-            int read = inputStream.read(data);
-            //logger.warn("File available size is ={}", read);
-            // 对字节数组Base64编码
-            BASE64Encoder encoder = new BASE64Encoder();
-            // 返回Base64编码过的字节数组字符串
-            return encoder.encode(data);
 
-        } catch (Throwable e) {
-            e.printStackTrace();
-            //logger.error("Read image failed, ");
-        } finally {
-            // 释放资源
-            data = null;
-            if (inputStream != null) {
-                try {
-                    inputStream.close();
-                } catch (Throwable throwable) {
-                   // logger.error("inputStream close failed, e=", throwable);
-                }
+    /**
+     * 第一个异常数据
+     * @throws Throwable
+     */
+    private void firstFaild() throws Throwable{
+        BloomFilter bloomFilter = new BloomFilter(10000, MisjudgmentRate.MIDDLE, null);
+
+        int failedCount = 0;
+        int dateCount = 0;
+        while (true) {
+            String data = UUID.randomUUID().toString();
+            dateCount++;
+            if(bloomFilter.checkExist(data)) {
+                System.out.println(String.format("Count : %s filedNum = %s failedData= %s", dateCount , ++failedCount, data));
+                break;
             }
+            bloomFilter.add(data);
         }
-        return null;
-    }
-
-    private String getImageForLink(String boardId) {
-        String imageBASE64Str = "";
-        try {
-            // 找之前二维码
-            String filePath = null;
-
-            List<String> fileNameWithPathList =
-                    getFileNameList(SERVICE_WORK_PATH + "imagePath/");
-            if (!CollectionUtils.isEmpty(fileNameWithPathList)) {
-                for (String fileNameWithPath : fileNameWithPathList) {
-                    if (fileNameWithPath.contains(boardId)) {
-                        filePath = fileNameWithPath;
-                        break;
-                    }
-                }
-            }
-            if (filePath == null) {
-                filePath = generateQRCodeStreamToFile(SERVICE_WORK_PATH + "imagePath/",
-                        boardId,
-                        boardId + "-" + System.currentTimeMillis());
-            }
-            imageBASE64Str = getImageBASE64Str(filePath);
-        } catch (Throwable throwable) {
-            //logger.error("Get file info failed, e=", throwable);
-        }
-        String resultStr = "";
-        if (imageBASE64Str != null) {
-            resultStr = "<div>请扫描下方二维码，处理圈子中的提案： 强势传送门</div>\n<div><img src='data:image/jpg;base64," + imageBASE64Str
-                    + "'/>";
-        }
-
-        return resultStr;
     }
 }
---
+++ src/main/java/com/dawei/test/demo/bloomfilter/BloomFilter.java
@@ -0,0 +1,147 @@
+package com.dawei.test.demo.bloomfilter;
+
+import java.io.Serializable;
+import java.util.BitSet;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * 布隆过滤器
+ *
+ * @author by Dawei on 2019/11/23.
+ */
+public class BloomFilter implements Serializable {
+
+    private static final long serialVersionUID = 8746284993613083826L;
+
+    private final int[] hashSeeds;
+
+    //过滤器长度 容量
+    private final int size;
+
+    //数据存储集合
+    private final BitSet noteBook;
+
+    //设置误判率， 通过误判率来调整hash次数
+    private final MisjudgmentRate misjudgmentRate;
+
+    //存储计数器
+    private final AtomicInteger usedCount = new AtomicInteger(0);
+
+    private final Double maxCoverFactor;
+
+
+    /**
+     * 设置过滤器容量
+     */
+    public BloomFilter(int filterSize) {
+        this(filterSize, MisjudgmentRate.MIDDLE, null);
+    }
+
+
+    public BloomFilter(int filterSize, MisjudgmentRate misjudgmentRate, Double maxCoverFactor) {
+
+        this.hashSeeds = misjudgmentRate.getHashSeeds();
+
+        long bizSize = this.hashSeeds.length * filterSize;
+        if (bizSize < 0 || bizSize > Integer.MAX_VALUE) {
+            throw new RuntimeException("filter size illegal");
+        }
+        this.size = (int) bizSize;
+        this.noteBook = new BitSet(size);
+        this.misjudgmentRate = misjudgmentRate;
+        this.maxCoverFactor = maxCoverFactor;
+    }
+
+
+    public void add(String data) throws Exception {
+        checkFull();
+        for (int hashSeed : this.hashSeeds) {
+            int index = getHashCode(data, hashSeed);
+            setBitBookPoint(index);
+        }
+    }
+
+    public boolean checkExist(String data) {
+        for (int hashSeed : this.hashSeeds) {
+            int index = getHashCode(data, hashSeed);
+            if (!this.noteBook.get(index)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean addIfNoExist(String data) throws Exception {
+        checkFull();
+        boolean exist = false;
+        for (int hashSeed : this.hashSeeds) {
+            int index = getHashCode(data, hashSeed);
+            if (!this.noteBook.get(index)) {
+                exist = true;
+                setBitBookPoint(index);
+            }
+        }
+        return exist;
+    }
+
+    public void clear() {
+        this.usedCount.set(0);
+        this.noteBook.clear();
+    }
+
+    private void checkFull() {
+
+        if (this.maxCoverFactor != null) {
+            if (this.maxCoverFactor <= getUseCountRate()) {
+                throw new RuntimeException("Out set max cover factor");
+            }
+        }
+    }
+
+
+    private double getUseCountRate() {
+        return this.usedCount.get() / this.size;
+    }
+
+    private void setBitBookPoint(int index) {
+        this.usedCount.incrementAndGet();
+        this.noteBook.set(index, true);
+    }
+
+    private int getHashCode(String date, int hashSeed) {
+        char[] dataChars = date.toCharArray();
+        int hash = 0;
+        if (dataChars.length > 0) {
+            for (int i = 0; i < dataChars.length; i++) {
+                //瞎捣鼓的 但是真的是快了不少
+                hash = (hash = i * hash + hashSeed + dataChars[i]) ^ (hash >>> 16);
+            }
+        }
+        hash = hash * hashSeed % this.size;
+        //防止溢出int 值 结果溢出成为复数
+        return Math.abs(hash);
+    }
+
+    /**
+     * 误判率设置 是 空间与正确率的博弈
+     */
+    public enum MisjudgmentRate {
+
+        VERY_SMALL(new int[]{2, 3, 5, 7}),
+        SMALL(new int[]{2, 3, 5, 7, 11, 13, 17, 19}),
+        MIDDLE(new int[]{2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53}),
+        HIGH(new int[]{2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59,
+            61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131});
+
+
+        private int[] hashSeeds;
+
+        MisjudgmentRate(int[] hashSeeds) {
+            this.hashSeeds = hashSeeds;
+        }
+
+        public int[] getHashSeeds() {
+            return hashSeeds;
+        }
+    }
+}
