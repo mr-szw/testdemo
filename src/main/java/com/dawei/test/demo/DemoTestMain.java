0d46b761f236f53899a4d3301aaff6eb4417e917
sunzhiwei <sunzhiwei@daojia-inc.com>
2019-11-27 22:49:40 +0800

    test

--- src/main/java/com/dawei/test/demo/DemoTestMain.java
+++ src/main/java/com/dawei/test/demo/DemoTestMain.java
@@ -1,8 +1,5 @@
 package com.dawei.test.demo;
 
-import com.google.common.hash.BloomFilter;
-import com.google.common.hash.Funnels;
-import org.apache.http.Consts;
 import org.junit.Test;
 
 /**
@@ -14,7 +11,6 @@ public class DemoTestMain {
     @Test
     public void testMethod() throws Throwable {
 
-        BloomFilter<String> bloomFilter = BloomFilter.create(Funnels.stringFunnel(Consts.UTF_8), 1);
 
     }
 }
