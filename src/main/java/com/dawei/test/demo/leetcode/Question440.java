f0159a447820f048191b160cc29e62d5ddab8870
sunzhiwei-win <sunzhiwei1@xiaomi.com>
2020-08-27 00:54:12 +0800

    add question

---
+++ src/main/java/com/dawei/test/demo/leetcode/Question440.java
@@ -0,0 +1,25 @@
+package com.dawei.test.demo.leetcode;
+
+/**
+ * https://leetcode-cn.com/problems/k-th-smallest-in-lexicographical-order/https://leetcode-cn.com/problems/k-th-smallest-in-lexicographical-order/
+ * 字典序的第K小数字
+ * @author sinbad on 2020/08/26.
+ */
+public class Question440 {
+
+ 	//关于topkey问题
+
+	public static void main(String[] args) {
+		System.out.println( new Question440().findKthNumber(215, 6));
+	}
+
+	public int findKthNumber(int n, int k) {
+		return 0;
+	}
+
+
+
+
+
+
+}
