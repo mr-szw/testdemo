b4a57e9026288a157219219293dccd8e71e78383
sinbad <sunzhiwei1@xiaomi.com>
2020-09-04 02:45:24 +0800

    add quesue

--- src/main/java/com/dawei/test/demo/leetcode/Question51.java
+++ src/main/java/com/dawei/test/demo/leetcode/Question51.java
@@ -1,44 +1,98 @@
 package com.dawei.test.demo.leetcode;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import com.google.gson.Gson;
 
 /**
  * N 皇后
  * https://leetcode-cn.com/problems/n-queens/
  *
  * @author sinbad on 2020/09/2.
+ *
+ * 目前处理结果 仅能返回一次的计算结果
  */
 public class Question51 {
 
 
 	public static void main(String[] args) {
 
-		new Question51().solveNQueens(8);
+		System.out.println(new Gson().toJson(new Question51().solveNQueens(4)));
 	}
-
 
 	public List<List<String>> solveNQueens(int n) {
 
+
+		int[] history = new int[n];
+
 		int[][] position = new int[n][n];
 
-		putPoint(position, 0, 0);
 
-		for (int i = 1; i < n; i++) {
-			for (int j = 0; j < n; j++) {
+		List<List<String>> result = new ArrayList<>();
 
-				if (position[i][0] == 1 || position[0][j] == 1) {
-					continue;
-				}
-				boolean canPut = checkCanPut(position, i, j);
-				if (canPut) {
-					putPoint(position, i, j);
-					break;
-				}
-			}
-		}
-		return null;
+		print(position, null);
+		checkRow(result, n, position, 0, history, true);
+
+//		int temp = 0;
+//
+//
+//		while (history[0] < n) {
+////
+//			temp++;
+//		}
+
+
+		return result;
 
 	}
+
+	private void checkRow(List<List<String>> result, int n, int[][] position, int row, int[] historyCol, boolean next) {
+		if (row == n) {
+			recordResult(position, result);
+			return;
+		}
+		int j = 0;
+		if (!next) {
+			j = historyCol[row];
+		}
+		boolean canPut = false;
+		for (; j < n; j++) {
+			if (position[row][0] == 1 || position[0][j] == 1) {
+				continue;
+			}
+			canPut = checkCanPut(position, row, j);
+			if (canPut) {
+				break;
+			}
+		}
+		if (!canPut) {
+			row = row -1;
+			putPoint(position, row, historyCol[row], false);
+			historyCol[row] = historyCol[row] + 1;
+			checkRow(result, n, position, row, historyCol, false);
+		} else {
+			historyCol[row] = j;
+			putPoint(position, row, j, true);
+			checkRow(result, n, position, row + 1, historyCol, true);
+		}
+	}
+
+
+	public void recordResult(int[][] position, List<List<String>> result){
+		List<String> subResultList = new ArrayList<>();
+		for (int[] ints : position) {
+			StringBuilder stringBuilder = new StringBuilder();
+			for (int value : ints) {
+				stringBuilder.append(value == 1 ? "Q" : ".");
+			}
+			subResultList.add(stringBuilder.toString());
+
+		}
+		result.add(subResultList);
+	}
+
+//}
 
 
 	private boolean checkCanPut(int[][] position, int row, int col) {
@@ -46,7 +100,7 @@ public class Question51 {
 	}
 
 	//放入皇后
-	private void putPoint(int[][] position, int row, int col) {
+	private void putPoint(int[][] position, int row, int col, boolean putFlag) {
 		int length = position.length;
 
 
@@ -56,7 +110,11 @@ public class Question51 {
 		//向上
 		while (tempRow >= 0) {
 			if (position[tempRow][col] != 1) {
-				position[tempRow][col] = 2;
+				if (putFlag) {
+					position[tempRow][col] += 2;
+				} else {
+					position[tempRow][col] -= 2;
+				}
 			}
 			tempRow--;
 		}
@@ -65,7 +123,11 @@ public class Question51 {
 		//向下
 		while (tempRow < length) {
 			if (position[tempRow][col] != 1) {
-				position[tempRow][col] = 2;
+				if (putFlag) {
+					position[tempRow][col] += 2;
+				} else {
+					position[tempRow][col] -= 2;
+				}
 			}
 			tempRow++;
 		}
@@ -75,7 +137,11 @@ public class Question51 {
 		//向上
 		while (tempCol >= 0) {
 			if (position[row][tempCol] != 1) {
-				position[row][tempCol] = 2;
+				if (putFlag) {
+					position[row][tempCol] += 2;
+				} else {
+					position[row][tempCol] -= 2;
+				}
 			}
 
 			tempCol--;
@@ -85,7 +151,11 @@ public class Question51 {
 		//向下
 		while (tempCol < length) {
 			if (position[row][tempCol] != 1) {
-				position[row][tempCol] = 2;
+				if (putFlag) {
+					position[row][tempCol] += 2;
+				} else {
+					position[row][tempCol] -= 2;
+				}
 			}
 			tempCol++;
 		}
@@ -96,7 +166,11 @@ public class Question51 {
 		//右上
 		while (tempRow >= 0 && tempCol < length) {
 			if (position[tempRow][tempCol] != 1) {
-				position[tempRow][tempCol] = 2;
+				if (putFlag) {
+					position[tempRow][tempCol] += 2;
+				} else {
+					position[tempRow][tempCol] -= 2;
+				}
 			}
 			tempRow--;
 			tempCol++;
@@ -106,7 +180,11 @@ public class Question51 {
 		//左上
 		while (tempRow < length && tempCol >= 0) {
 			if (position[tempRow][tempCol] != 1) {
-				position[tempRow][tempCol] = 2;
+				if (putFlag) {
+					position[tempRow][tempCol] += 2;
+				} else {
+					position[tempRow][tempCol] -= 2;
+				}
 			}
 			tempRow++;
 			tempCol--;
@@ -116,7 +194,11 @@ public class Question51 {
 		//右下
 		while (tempRow < length && tempCol < length) {
 			if (position[tempRow][tempCol] != 1) {
-				position[tempRow][tempCol] = 2;
+				if (putFlag) {
+					position[tempRow][tempCol] += 2;
+				} else {
+					position[tempRow][tempCol] -= 2;
+				}
 			}
 			tempRow++;
 			tempCol++;
@@ -126,25 +208,48 @@ public class Question51 {
 		//左上
 		while (tempRow >= 0 && tempCol >= 0) {
 			if (position[tempRow][tempCol] != 1) {
-				position[tempRow][tempCol] = 2;
+				if (putFlag) {
+					position[tempRow][tempCol] += 2;
+				} else {
+					position[tempRow][tempCol] -= 2;
+				}
 			}
 			tempRow--;
 			tempCol--;
 		}
 
-
-		print(position);
+		if (!putFlag) {
+			position[row][col] = 0;
+		}
+		print(position, putFlag);
 
 	}
 
 
-	private void print(int[][] position) {
+	int num = 0;
+
+	private void print(int[][] position, Boolean putFlag) {
 		int length = position.length;
+		if (putFlag != null) {
+			if (putFlag) {
+				System.out.println("############### PUT  ############ num=" + num++);
+			} else {
+				System.out.println("--------- back    --------- back=" + --num);
+			}
+		}
 		for (int i = 0; i < length; i++) {
 			for (int j = 0; j < length; j++) {
 				System.out.print(" " + position[i][j]);
 			}
 			System.out.println(" ");
+		}
+
+		if (putFlag != null) {
+			if (putFlag) {
+				System.out.println("############### PUT  ############ num=" + num);
+			} else {
+				System.out.println("--------- back    --------- back=" + num);
+			}
 		}
 		System.out.println(" ");
 	}
