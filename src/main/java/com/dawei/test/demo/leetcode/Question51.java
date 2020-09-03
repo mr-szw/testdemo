dbfd7bac42b2ce25d613672f66d2a18d6022e456
sunzhiwei1 <sunzhiwei1@xiaomi.com>
2020-09-03 17:37:42 +0800

    add question

---
+++ src/main/java/com/dawei/test/demo/leetcode/Question51.java
@@ -0,0 +1,23 @@
+package com.dawei.test.demo.leetcode;
+
+import java.util.List;
+
+/**
+ *  N 皇后
+ * https://leetcode-cn.com/problems/n-queens/
+ * @author sinbad on 2020/09/2.
+ *
+ */
+public class Question51 {
+
+
+	public static void main(String[] args) {
+		String grid = "";
+		System.out.println(new Question51().solveNQueens(grid));
+	}
+
+
+	public List<List<String>> solveNQueens(int n) {
+
+	}
+}
