e62054cf2533f706205c92c3629cf20f3bd37242
szw-linux <sunzhiwei1@xiaomi.com>
2020-09-12 19:03:18 +0800

    add question

--- src/main/java/com/dawei/test/demo/leetcode/Question216.java
+++ src/main/java/com/dawei/test/demo/leetcode/Question216.java
@@ -26,39 +26,27 @@ public class Question216 {
 		System.out.println(new Gson().toJson(new Question216().combinationSum3(3, 7)));
 	}
 
+
 	public List<List<Integer>> combinationSum3(int k, int n) {
 		List<List<Integer>> resultList = new ArrayList<>();
 		Stack<Integer> subStack = new Stack<>();
-		boolean[] stop = new boolean[]{true};
-		search(1, k, n, resultList, subStack, stop);
+		search(1, k, n, 0, resultList, subStack);
 		return resultList;
 	}
-	//三个数  k个数字  n -> target
 
-
-	public void search(int start, int needNum, int target, List<List<Integer>> resultList, Stack<Integer> subStack, boolean[] stop) {
-		if (target == 0 && needNum == 0) {
+	public void search(int start, int len, int target, int index, List<List<Integer>> resultList, Stack<Integer> subStack) {
+		if (target == 0 && len == subStack.size()) {
 			resultList.add(new ArrayList<>(subStack));
-			System.out.println("OKOKOKOKOKOKOKOKOKOKOKOK");
-			Integer pop = subStack.pop();
-			System.out.println("pop  >> " + pop);
 			return;
 		}
-		if (target - start < 0) {
 
-			return;
-		}
-		for (int i = start; i < 9; i++) {
-			subStack.push(i);
-			System.out.println("Push >> " + i);
-			search(i + 1, needNum - 1, target - i, resultList, subStack, stop);
-			Integer pop = subStack.pop();
-			System.out.println("pop  >> " + pop);
-			if (!stop[0]) {
-				stop[0] = true;
-				break;
+		for (int i = start; i <= 9 && i <= target; i++) {
+			if (target - start < 0) {
+				return;
 			}
-			System.out.println("############");
+			subStack.push(i);
+			search(i + 1, len, target - i, index + 1, resultList, subStack);
+			subStack.pop();
 		}
 	}
 
