f8ff2ae56698e498268694746c019850bbfd3c34
sunzhiwei1 <sunzhiwei1@xiaomi.com>
2020-09-13 17:23:29 +0800

    ad quetion:

--- src/main/java/com/dawei/test/demo/leetcode/Question79.java
+++ src/main/java/com/dawei/test/demo/leetcode/Question79.java
@@ -1,7 +1,12 @@
 package com.dawei.test.demo.leetcode;
 
+import java.util.Arrays;
+import java.util.BitSet;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Stack;
+
+import org.checkerframework.checker.units.qual.A;
 
 import com.google.gson.Gson;
 
@@ -15,21 +20,82 @@ public class Question79 {
 
 
 	public static void main(String[] args) {
+		//char[][] board = new char[][]{{'A', 'B', 'C', 'E'}, {'S', 'F', 'C', 'S'}, {'A', 'D', 'E', 'E'}};
+		char[][] board = new char[][]{{'C', 'A', 'A'},{'A','A','A'},{'B','C','D'}};
+		String word = "AAB";
+		System.out.println(new Question79().exist(board, word));
 	}
+
 	public boolean exist(char[][] board, String word) {
 		char[] chars = word.toCharArray();
 		char firstNum = chars[0];
+		int height = board.length; // 外层
+		int weight = board[0].length; //内层 4
+		boolean[][] history = new boolean[height][weight];
+		for (int nowHeight = 0; nowHeight < height; nowHeight++) {
+			for (int nowWeigh = 0; nowWeigh < weight; nowWeigh++) {
+				if (board[nowHeight][nowWeigh] == firstNum) {
+					history[nowHeight][nowWeigh] = true;
+					boolean exist = exist(board, chars, 1, nowWeigh, nowHeight, weight, height, history);
+					history[nowHeight][nowWeigh] = false;
+					if (exist) {
+						return true;
+					}
+				}
+			}
+		}
+		return false;
 
+	}
 
-		for (int i = 0; i < board.length; i++) {
-			for (int i1 = 0; i1 < board[i].length; i1++) {
+	public boolean exist(char[][] board, char[] chars, int nextWordIndex, int nowWeigh, int nowHeight, int weigh, int height, boolean[][] history) {
+
+		if (nextWordIndex == chars.length) {
+			return true;
+		}
+
+		//右
+		if (nowWeigh + 1 < weigh) {
+			if (chars[nextWordIndex] == board[nowHeight][nowWeigh + 1] && !history[nowHeight][nowWeigh + 1]) {
+				history[nowHeight][nowWeigh + 1] = true;
+				boolean check =  exist(board, chars, nextWordIndex + 1, nowWeigh + 1, nowHeight, weigh, height, history);
+				if(check) {
+					return true;
+				}
 
 			}
 		}
-
-
-		final boolean b = false;
-		return b;
-
+		//左
+		if (nowWeigh - 1 >= 0) {
+			if (chars[nextWordIndex] == board[nowHeight][nowWeigh - 1] && !history[nowHeight][nowWeigh - 1]) {
+				history[nowHeight][nowWeigh - 1] = true;
+				boolean check =   exist(board, chars, nextWordIndex + 1, nowWeigh - 1, nowHeight, weigh, height, history);
+				if(check) {
+					return true;
+				}
+			}
+		}
+		//下
+		if (nowHeight + 1 < height) {
+			if (chars[nextWordIndex] == board[nowHeight + 1][nowWeigh] && !history[nowHeight + 1][nowWeigh]) {
+				history[nowHeight + 1][nowWeigh] = true;
+				boolean check =   exist(board, chars, nextWordIndex + 1, nowWeigh, nowHeight + 1, weigh, height, history);
+				if(check) {
+					return true;
+				}
+			}
+		}
+		//上
+		if (nowHeight - 1 >= 0) {
+			if (chars[nextWordIndex] == board[nowHeight - 1][nowWeigh] && !history[nowHeight - 1][nowWeigh]) {
+				history[nowHeight - 1][nowWeigh] = true;
+				boolean check =   exist(board, chars, nextWordIndex + 1, nowWeigh, nowHeight - 1, weigh, height, history);
+				if(check) {
+					return true;
+				}
+			}
+		}
+		history[nowHeight][nowWeigh] = false;
+		return false;
 	}
 }
