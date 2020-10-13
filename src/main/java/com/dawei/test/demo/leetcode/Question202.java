b45feee6daa8b339ca1bb60d14ca401f8d090a0e
sinbad <sunzhiwei1@xiaomi.com>
2020-10-13 22:42:55 +0800

    add question

---
+++ src/main/java/com/dawei/test/demo/leetcode/Question202.java
@@ -0,0 +1,44 @@
+package com.dawei.test.demo.leetcode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 202. 快乐数
+ * https://leetcode-cn.com/problems/happy-number/
+ *
+ * @author sinbad on 2020/10/13.
+ * 我不快乐
+ */
+public class Question202 {
+
+
+	public static void main(String[] args) {
+
+		System.out.println(new Question202().isHappy(2));
+	}
+
+	public boolean isHappy(int n) {
+		Set<Integer> hasUseSet = new HashSet<>();
+		return calculation(n, hasUseSet);
+	}
+
+
+	private boolean calculation(int n, Set<Integer> hasUseSet) {
+
+		if (!hasUseSet.add(n)) {
+			return false;
+		}
+		if (n == 1) {
+			return true;
+		}
+		int sum = 0;
+		do {
+			int num = n - (n / 10) * 10;
+			n = n / 10;
+			sum += num * num;
+		} while (n > 0);
+		return isHappy(sum);
+	}
+
+}
