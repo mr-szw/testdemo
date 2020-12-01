f618652036fa6b2d6ba14553c0de91d4c56c0091
sunzhiwei1 <sunzhiwei1@xiaomi.com>
2020-12-01 10:22:39 +0800

    add question

---
+++ src/main/java/com/dawei/test/demo/leetcode/Question767.java
@@ -0,0 +1,21 @@
+package com.dawei.test.demo.leetcode;
+
+import com.google.gson.Gson;
+
+/**
+ * 767. 重构字符串 https://leetcode-cn.com/problems/reorganize-string/ 动态规划
+ *
+ * @author sinbad on 2020/09/11.
+ */
+public class Question767 {
+
+	public static void main(String[] args) {
+		System.out.println(new Gson().toJson(new Question767().reorganizeString("3, 7")));
+	}
+
+	public String reorganizeString(String S) {
+		return "";
+
+	}
+
+}
